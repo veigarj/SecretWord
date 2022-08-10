edd1a87c40d240c573c35eac039543337006501a
Glauco Veiga <101466539+veigarj@users.noreply.github.com>
2022-08-10 17:41:23 -0300

    2.0

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import StartScreen from "./component/StartScreen";
 export default function App() {
   return (
     <div className="App">
-      <h2>Secret Word</h2>
       <StartScreen />
     </div>
   );
