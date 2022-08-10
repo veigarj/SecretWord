9714fc81bba797a7cc1f9e57e0c5a79ce59c31d9
Glauco Veiga <101466539+veigarj@users.noreply.github.com>
2022-08-10 17:53:47 -0300

    1.5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 // Style / CSS
 import "./styles.css";
 
+// React
+import { useCallback, useState, useEffect } from "react";
+
+// Dados
+import { wordsList } from "./data/words";
+
 // Components
 import StartScreen from "./component/StartScreen";
 
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" }
+];
+
 export default function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+
   return (
     <div className="App">
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <StartScreen />}
       <StartScreen />
     </div>
   );
