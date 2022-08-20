aee4b63928b96a1d560b664728eedd1ecf396001
Glauco Veiga <veiga.cg@gmail.com>
2022-08-20 10:14:23 -0300

    new

--- src/data/words.js
+++
@@ -1,8 +0,0 @@
-export const wordsList = {
-  carro: ["Motor", "Porta", "Capo", "Pneu", "Antena"],
-  fruta: ["Banana", "Maca", "Pera", "Mamao", "Laranja"],
-  corpo: ["Braco", "Perna", "Cerebro", "Pescoço", "olhos"],
-  computador: ["Mouser", "Teclado", "Monitor", "Gabinete"],
-  programador: ["Linguagem", "Framework", "JavaScript", "React"],
-  alimento: ["Arroz", "Feijao", "Carne", "Leite", "Ovo"]
-};
